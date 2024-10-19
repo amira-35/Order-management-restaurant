ba1aaed8a016915c3d17ebde9a8d5d3e44b49b44
amira-35 <83829837+amira-35@users.noreply.github.com>
2024-10-19 22:14:19 +0100

    Initial commit

---
+++ Exe/Aide Gestion des commandes d'un restaurant/FR/mapid.h
@@ -0,0 +1,9 @@
+#define LivreGestion des commandes d'un restaurant	0x00000001
+#define DescFen	0x00000002
+#define Fenêtre_FEN_Nos_services	0x00000003
+#define Fenêtre_FEN_BIENVENUE	0x00000004
+#define Fenêtre_FEN_Information_Client	0x00000005
+#define Fenêtre_FEN_conf	0x00000006
+#define Fenêtre_FEN_login	0x00000007
+#define Fenêtre_FEN_resto	0x00000008
+
